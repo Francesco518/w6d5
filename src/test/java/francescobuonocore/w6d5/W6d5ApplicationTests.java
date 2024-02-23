3b65113f721c47450e02c4dca84afed9c3690e8f
Francesco518 <fbuonocore655@gmail.com>
2024-02-23 10:29:46 +0100

    Project Java Spring W6D5

---
+++ src/test/java/francescobuonocore/w6d5/W6d5ApplicationTests.java
@@ -0,0 +1,13 @@
+package francescobuonocore.w6d5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W6d5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
